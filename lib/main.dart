dffd2ab44c539e46f59153cd8a08fd35ed5a52f0
Rajan Sharma <rajanshxrma@users.noreply.github.com>
2026-04-19 13:16:11 -0400

    created flutter project with http dependency

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'quiz_screen.dart';
+
+void main() {
+  runApp(const QuizApp());
+}
+
+class QuizApp extends StatelessWidget {
+  const QuizApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quiz App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorSchemeSeed: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: const QuizScreen(),
+    );
+  }
+}
